22ac4ab2f6cf3f3757d43e8745424cd14a484c8a
André Galdino <andregaldino@users.noreply.github.com>
2019-11-16 14:47:37 -0300

    enable cors (#2)

--- src/functions/graphql.js
+++ src/functions/graphql.js
@@ -15,4 +15,9 @@ const server = new ApolloServer({
   introspection: true,
 });
 
-exports.handler = server.createHandler();
+exports.handler = server.createHandler({
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+});
